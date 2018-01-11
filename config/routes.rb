050c8fcd5d9c82ab3293e3e1e483fa456e86d210
Tatsuya Ichikawa <>
2018-01-11 17:48:44 +0900

    Finish signup-function

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
 
   get    '/search',  to: 'communities#search'
   get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
   # get    '/login',   to: 'sessions#new'
   # post   '/login',   to: 'sessions#create'
   # delete '/logout',  to: 'sessions#destroy'
-  # resouces :users
+  resources :users
   # resouces :tickets
   # resouces :talks, only: [:index, :show]
 
