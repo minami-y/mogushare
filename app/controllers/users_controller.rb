70f34aef5811adf4972928867aabe0dae8185048
Tatsuya Ichikawa <>
2018-01-11 20:04:03 +0900

    Implement basic login and logout function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "ユーザー登録が完了しました"
       # 仮置き　実際はタイムラインにリダイレクト
       redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'communities#index'
 
-  get    '/search',  to: 'communities#search'
+  post    '/search',  to: 'communities#search'
   get    '/signup',  to: 'users#new'
   post   '/signup',  to: 'users#create'
-  # get    '/login',   to: 'sessions#new'
-  # post   '/login',   to: 'sessions#create'
-  # delete '/logout',  to: 'sessions#destroy'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
   # resouces :tickets
   # resouces :talks, only: [:index, :show]
