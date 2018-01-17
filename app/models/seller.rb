7d2e446dacaa9a67314d5a3388cc91b50c45f92c
Tatsuya Ichikawa <>
2018-01-17 18:29:59 +0900

    Modify seller-form

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,4 +1,7 @@
 class Seller < ApplicationRecord
   belongs_to :user, optional: true
   has_many :tickets
+  validates :official_name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
 end
