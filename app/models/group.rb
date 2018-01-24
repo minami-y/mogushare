8a8d931331c07c6ff97059076cc0ee09bdf0ffa0
Tatsuya Ichikawa <>
2018-01-24 20:44:13 +0900

    Modify talk_index page

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,12 @@ class Group < ApplicationRecord
   has_many :user_groups
   has_many :users, through: :user_groups
   has_many :talks
+
+  def latest_messages(group)
+    if group.talks.last.try(:message).present?
+      group.talks.last.try(:message)
+    else
+      "まだメッセージがありません。"
+    end
+  end
 end
