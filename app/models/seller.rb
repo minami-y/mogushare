e046929a40241a2d65c5d9a4a102cfb06e353321
Tatsuya Ichikawa <>
2018-03-09 00:52:53 +0900

    Correct edit-profile-form

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -2,7 +2,7 @@ class Seller < ApplicationRecord
   belongs_to :user, optional: true
   has_many :tickets
   has_many :orders
-  mount_uploader :image, ImageUploader
+  mount_uploader :photo, ImageUploader
   validates :photo, presence: true
   validates :self_introduction, presence: true
 end
