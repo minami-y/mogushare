d78820fb7e7d4baf58b1f8ad9db684212dedbf28
Tatsuya Ichikawa <>
2018-03-09 01:05:09 +0900

    Edit seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require "csv"
 
 Prefectural.delete_all # 重複するとERRORになる為、seedの度に再作成している。
 namelist = Array.new
-CSV.foreach('db/prefecturals_name_seed23.csv') do |row|
+CSV.foreach('db/prefecturals_name_seed.csv') do |row|
   record = {
     :id              => row[0].to_i,
     :name            => row[1],
@@ -14,7 +14,7 @@ CSV.foreach('db/prefecturals_name_seed23.csv') do |row|
 end
 
 Area.delete_all
-CSV.foreach('db/postal_code_seed23.csv') do |row|
+CSV.foreach('db/postal_code_seed.csv') do |row|
   record = {
     :postal_code     => row[0],
     :prefectural     => namelist[row[1].to_i], # オブジェクトを紐付け
