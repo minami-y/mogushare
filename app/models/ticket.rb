f8be2dc691464ed70410f12afe982c0ee13a4bd2
Ray Takahashi <asirei1012@gmail.com>
2018-03-09 08:33:31 +0900

    feat: Edit validation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,7 +3,7 @@ class Ticket < ApplicationRecord
   belongs_to :user, optional: true
   has_many :shares, dependent: :destroy
   accepts_nested_attributes_for :shares, allow_destroy: true
-  validates :message, presence: true
   validates :event_date, presence: true
   validates :expiration_date, presence: true
+  validates :genre, presence: true
 end
