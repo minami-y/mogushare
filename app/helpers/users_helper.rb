7b00c2d36ea1c290c4e80ad6c9622a61899e8789
d_saito <saito@furari.me>
2018-03-13 01:12:52 +0900

    update png files

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
     if user.seller.present? && user.seller.photo.present?
       image_tag user.seller.photo, class: "user-image"
     else
-      image_tag "blank.jpg", class: "user-image"
+      image_tag "blank.png", class: "user-image"
     end
   end
 end
