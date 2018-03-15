eaedfc506bb97d75aec1848be5aac147665af1d5
Tatsuya Ichikawa <>
2018-03-16 01:15:33 +0900

    fix order_form and chat views

--- app/assets/javascripts/order_form.js
+++ app/assets/javascripts/order_form.js
@@ -1,5 +1,9 @@
 $(document).on('turbolinks:load', function() {
-  $("select").change(function(){
+    var num = 0;
+  $('.plus').on('click', function(){
+    num++;
+    $(this).prev().val(num);
+    console.log("aaa");
     var array = [];
     for(var i=0; i < $(".ticket-item-menu").length; i++){
     var item_price = $(".ticket-item-menu").eq(i).data("price");
@@ -14,5 +18,26 @@ $(document).on('turbolinks:load', function() {
     }
 
     $("#item-price-total").val(total);
-  });
+    });
+  $('.minus').on('click', function(){
+    num--;　num > 0;
+    if(num < 0) num = 0;
+    $(this).prev().prev().val(num);
+    console.log("aaa");
+    var array = [];
+    for(var i=0; i < $(".ticket-item-menu").length; i++){
+    var item_price = $(".ticket-item-menu").eq(i).data("price");
+    var item_select = $("select option:selected").eq(i).val();
+    array.push(item_price * item_select);
+    $(".item-price-total").eq(i).val(array[i]);
+    }
+
+    var total = 0;
+    for(var j = 0; j < array.length; j++){
+      total += array[j];
+    }
+
+    $("#item-price-total").val(total);
+    });
+
 });
