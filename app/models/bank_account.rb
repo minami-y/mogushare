e31facf065b94fc192f2f1fa484e78e31ce3c90d
Ray Takahashi <asirei1012@gmail.com>
2018-03-19 19:45:31 +0900

    feat: Add validation

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,3 +1,5 @@
 class BankAccount < ApplicationRecord
   belongs_to :seller, optional: true
+
+  validates :bank, :account_type, :branch_code, :account_number, :name, :bank_code, presence: true
 end
