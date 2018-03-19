c1ce966895a51d769874c5481317523f451e3eb9
Ray Takahashi <asirei1012@gmail.com>
2018-03-19 19:45:40 +0900

    feat: Define enum

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -2,4 +2,6 @@ class BankAccount < ApplicationRecord
   belongs_to :seller, optional: true
 
   validates :bank, :account_type, :branch_code, :account_number, :name, :bank_code, presence: true
+
+  enum account_type: %i(normal current)
 end
