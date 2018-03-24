e1181f88cf73a69697733e657a16d75a7c4e245d
Tatsuya Ichikawa <>
2018-03-25 01:04:50 +0900

    Add js for photo-preview

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -41,3 +41,24 @@
 //     });
 
 // });
+
+
+
+
+$(document).on('turbolinks:load', function() {
+    $('form').on('change', 'input[type="file"]', function(){
+        if (!this.files.length) {
+            return;
+        }
+        var file = this.files[0],           //画像１つのみ選択
+            image = $(".preview"),
+            fileReader = new FileReader();
+        // 読み込みが完了した際のイベントハンドラ。imgのsrcにデータセット
+        fileReader.onload = function(event) {
+            // 読み込んだデータをimgに設定
+            image.children('img').attr('src', event.target.result);
+        };
+        // 画像読み込み
+        fileReader.readAsDataURL(file);
+    });
+});
