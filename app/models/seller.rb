283eb9a3f43561c24e1191c20abe111512c3f198
Ray Takahashi <asirei1012@gmail.com>
2018-03-26 04:38:13 +0900

    feat: Validations for personal information

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -8,5 +8,17 @@ class Seller < ApplicationRecord
   validates :photo, presence: true
   validates :self_introduction, presence: true
 
+  validates :address_kana_state, :address_kana_city, :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[ぁ-んー－]+\z/ }
+
+  validates :address_kana_town, presence: true, format: { with: /\A[ぁ-ん][ぁ-ん０-９0-9ー－-]+\z/ }
+
+  validates :address_kanji_state, :address_kanji_city, :address_kanji_town, :address_kanji_line, :address_kana_line, :first_name_kanji, :last_name_kanji, :gender, :date_of_birth, presence: true
+
+  validates :phone_number, presence: true, format: { with: /\A\d{10}\z|\A\d{11}\z/ }
+
+  validates :postal_code, presence: true, inclusion: { in: Area.pluck(:postal_code) }
+
   accepts_nested_attributes_for :bank_account
+
+  enum gender: %i(male female)
 end
