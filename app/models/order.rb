8cecbeb58f017fc7128bb3cae433473ba97b3024
Ray Takahashi <asirei1012@gmail.com>
2018-03-30 17:30:25 +0900

    feat: Delete order_details when an order is deleted

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :seller
-  has_many :order_details
+  has_many :order_details, dependent: :destroy
   accepts_nested_attributes_for :order_details, allow_destroy: true
 end
