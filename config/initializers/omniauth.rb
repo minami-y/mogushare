b96fa8f5669e7f191be6c4e91745c19fde2cc94e
Tatsuya Ichikawa <>
2018-03-31 01:22:26 +0900

    fix omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
+OmniAuth.config.full_host = "https://mogushare.net"
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
