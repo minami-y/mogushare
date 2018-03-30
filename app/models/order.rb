5ecb766500dea0d0c5ee13c5d5032ea15c419cb0
Ray Takahashi <asirei1012@gmail.com>
2018-03-30 17:30:34 +0900

    feat: Add validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,6 @@ class Order < ApplicationRecord
   belongs_to :seller
   has_many :order_details, dependent: :destroy
   accepts_nested_attributes_for :order_details, allow_destroy: true
+
+  validates :user_id, :seller_id, :stripe_charge_id, presence: true
 end
