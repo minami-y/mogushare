2e44bf8ba8e4730614664e49d1720940eee70bdb
nowin <imanaka754@gmail.com>
2018-04-04 11:40:23 +0000

    add validate and change clendar

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -2,6 +2,10 @@ class BankAccount < ApplicationRecord
   belongs_to :seller, optional: true
 
   validates :bank, :account_type, :branch_code, :account_number, :name, :bank_code, presence: true
+  validates :bank_code, length: { is: 4 }
+  validates :branch_code, length: { is: 3 }
+  validates :account_number, length: { in: 6..8 }
 
   enum account_type: %i(normal current)
+
 end
